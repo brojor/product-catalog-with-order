e051285681488182ffe5f804f9c8d26e453c4cac
brojor <bronislav.jordan@gmail.com>
2024-08-13 16:33:59 +0200

    feat(ProductItem): add AddToCartButton

---
+++ components/AddToCartButton.vue
@@ -0,0 +1,7 @@
+<template>
+	<button
+		class="border border-slate-600 rounded-full w-40 h-10 mx-auto -mt-5 bg-white text-sm inline-flex items-center justify-center gap-2 font-semibold">
+		<IconsAddToCart />
+		Add to Cart
+	</button>
+</template>
--- components/ProductItem.vue
+++ components/ProductItem.vue
@@ -4,7 +4,8 @@
 			<source media="(min-width: 640px)" :srcset="`/images/${product.imgName}-desktop.jpg`">
 			<img :src="`/images/${product.imgName}-mobile.jpg`" :alt="product.name" class="rounded-xl">
 		</picture>
-		<p class="text-slate-500 text-sm mt-4">{{ product.category }}</p>
+		<AddToCartButton />
+		<p class="text-slate-500 text-sm mt-1">{{ product.category }}</p>
 		<h2 class="font-semibold my-1">{{ product.name }}</h2>
 		<p class="font-semibold text-red-700">{{ formatPrice(product.price) }}</p>
 	</div>
