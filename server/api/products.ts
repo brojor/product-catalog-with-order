f60ac029e91bd1d8e1fe6612fbdcef0ae81c4ebf
brojor <bronislav.jordan@gmail.com>
2024-08-13 14:13:34 +0200

    initial commit

---
+++ server/api/products.ts
@@ -0,0 +1,67 @@
+export default defineEventHandler((event) => {
+	return [
+		{
+			id: 1,
+			imgName: 'waffle',
+			name: 'Waffle with Berries',
+			category: 'Waffle',
+			price: 6.5,
+		},
+		{
+			id: 2,
+			imgName: 'creme-brulee',
+			name: 'Vanilla Bean Crème Brûlée',
+			category: 'Crème Brûlée',
+			price: 7.0,
+		},
+		{
+			id: 3,
+			imgName: 'macaron',
+			name: 'Macaron Mix of Five',
+			category: 'Macaron',
+			price: 8.0,
+		},
+		{
+			id: 4,
+			imgName: 'tiramisu',
+			name: 'Classic Tiramisu',
+			category: 'Tiramisu',
+			price: 5.5,
+		},
+		{
+			id: 5,
+			imgName: 'baklava',
+			name: 'Pistachio Baklava',
+			category: 'Baklava',
+			price: 4.0,
+		},
+		{
+			id: 6,
+			imgName: 'meringue',
+			name: 'Lemon Meringue Pie',
+			category: 'Pie',
+			price: 5.0,
+		},
+		{
+			id: 7,
+			imgName: 'cake',
+			name: 'Red Velvet Cake',
+			category: 'Cake',
+			price: 4.5,
+		},
+		{
+			id: 8,
+			imgName: 'brownie',
+			name: 'Salted Caramel Brownie',
+			category: 'Brownie',
+			price: 4.5,
+		},
+		{
+			id: 9,
+			imgName: 'panna-cotta',
+			name: 'Vanilla Panna Cotta',
+			category: 'Panna Cotta',
+			price: 6.5,
+		},
+	]
+})
