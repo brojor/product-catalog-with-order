799edfd3993b910a0579082775b4820943f3560b
brojor <bronislav.jordan@gmail.com>
2024-08-15 08:16:04 +0200

    feat(ASummary): add shipping and payment

--- components/ASummary.vue
+++ components/ASummary.vue
@@ -5,18 +5,39 @@
 		<ul>
 			<li v-for="cartItem in cartStore.$state.items" :key="cartItem.product.id"
 				class="flex gap-4 pb-4 mb-4 border-b border-slate-300">
-				<img :src="`/images/${cartItem.product.imgName}-thumbnail.jpg`" :alt="cartItem.product.name" class="w-12 h-12 rounded">
+				<img :src="`/images/${cartItem.product.imgName}-thumbnail.jpg`" :alt="cartItem.product.name"
+					class="w-12 h-12 rounded">
 				<div class="text-xs">
 					<h3 class="text-sm font-semibold mb-3">{{ cartItem.product.name }}</h3>
 					<span class="text-red-700 font-semibold">{{ cartItem.quantity }}x</span>
-					<span class="ml-4 text-slate-500">@ {{formatPrice(cartItem.product.price)  }}</span>
+					<span class="ml-4 text-slate-500">@ {{ formatPrice(cartItem.product.price) }}</span>
 				</div>
-				<span class="ml-auto self-center font-semibold">{{ formatPrice(cartItem.product.price * cartItem.quantity) }}</span>
+				<span class="ml-auto self-center font-semibold">{{ formatPrice(cartItem.product.price * cartItem.quantity)
+					}}</span>
+			</li>
+			<li class="flex font-semibold pb-4 mb-4 border-b border-slate-300">
+				<span class="text-sm mr-2 text-slate-500">Payment:</span>
+				<span class="text-sm ">{{ servicesStore.$state.paymentMethod!.name }}</span>
+				<span class="ml-auto">{{ formatPrice(servicesStore.$state.paymentMethod!.price) }}</span>
+			</li>
+			<li class="flex font-semibold pb-4 mb-4 border-b border-slate-300">
+				<span class="text-sm mr-2 text-slate-500">Shipping:</span>
+				<span class="text-sm ">{{ servicesStore.$state.shippingMethod!.name }}</span>
+				<span class="ml-auto">{{ formatPrice(servicesStore.$state.shippingMethod!.price) }}</span>
 			</li>
 		</ul>
+		<div class="flex justify-between items-center">
+			<span class="font-semibold">Total:</span>
+			<span class="text-2xl font-bold">{{ formatPrice(totalPrice) }}</span>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 const cartStore = useCartStore()
+const servicesStore = useServicesStore()
+
+const totalPrice = computed(() => {
+	return cartStore.totalPrice + servicesStore.totalPrice
+})
 </script>
--- stores/services.ts
+++ stores/services.ts
@@ -15,4 +15,12 @@ export const useServicesStore = defineStore('services', {
 			this.shippingMethod = service
 		},
 	},
+
+	getters: {
+		totalPrice: (state) => {
+			const paymentPrice = state.paymentMethod?.price || 0
+			const shippingPrice = state.shippingMethod?.price || 0
+			return paymentPrice + shippingPrice
+		},
+	},
 })
